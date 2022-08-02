34111cf4d7ef7b34ce8211f2dc8c8e02fc418194
Matt Burns <matt.burns01@gmail.com>
2022-08-01 18:31:54 -0700

    psudo code

---
+++ index.js
@@ -0,0 +1,21 @@
+// require (inquirer)
+
+//global scope  create an empty array list to stor employee objects
+
+// gather MANAGER DATA
+    //inquirer.prompt()
+    // build manger object
+
+// gather ENGINEER DATA
+    //inquirer.prompt()
+    // build engineer object
+    
+// gather INTERN DATA
+    //inquirer.prompt()
+    //THEN build a response
+
+//ask for "what team member you would like to add" or "are you done"
+    //inquirer.prompt()
+    //then deciding wat function to call
+
+//generate the Html and write to a file
