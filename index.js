6ffd5431196463077c1741ba72cc3f37d106df3f
Matt Burns <matt.burns01@gmail.com>
2022-08-20 15:18:59 -0700

    created files needed for prodject

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // require (inquirer)
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Employee, Manager, Engineer, and Intern
 
 //global scope  create an empty array list to stor employee objects
+let teamProfile = [];
 
 // gather MANAGER DATA
     //inquirer.prompt()
@@ -17,5 +22,7 @@
 //ask for "what team member you would like to add" or "are you done"
     //inquirer.prompt()
     //then deciding wat function to call
+
+
 
 //generate the Html and write to a file
