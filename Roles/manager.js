41d3c4b43136a76bd9f0acbe1812cf0de7e09480
Matt Burns <matt.burns01@gmail.com>
2022-08-21 12:17:09 -0700

    built out manager.js file and attemt to extend to emloyee.js file just copied and pasted from engineer with some changes.

---
+++ Roles/manager.js
@@ -0,0 +1,23 @@
+// name
+// id
+// email
+// getName()
+// getId()
+// getEmail()
+// getRole()
+// In addition to Employee's properties and methods, Manager will also have the following:
+// officeNumber
+// getRole()—overridden to return 'Manager'
+
+const employee = require('/employee.js');
+
+class Manager extends Employee{
+    constructor (name, id, email, role, officeNumber){
+        super(employee)
+        this.github = github;
+    }
+    getOfficeNumber(){
+        return this.getOfficeNumber;
+    }
+}
+module.exports = Manager;
